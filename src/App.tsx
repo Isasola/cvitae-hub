454c10e3f1d883846de2528f970fa3dcaeb6d37d
Isaias Solaeche <98595918+Isasola@users.noreply.github.com>
2026-05-27 17:55:00 -0300

    Add AuthCallback route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'wouter'
 import Dashboard from './pages/Dashboard'
 import ProfileBuilder from './pages/ProfileBuilder'
 import JobMatcher from './pages/JobMatcher'
+import AuthCallback from './pages/AuthCallback'   // ← Nueva página
 
 export default function App() {
   return (
@@ -9,6 +10,9 @@ export default function App() {
       <Route path="/" component={Dashboard} />
       <Route path="/profile" component={ProfileBuilder} />
       <Route path="/match" component={JobMatcher} />
+      <Route path="/auth/callback" component={AuthCallback} />   {/* Nueva ruta */}
+      {/* Captura cualquier ruta con hash (importante para Supabase) */}
+      <Route path="/profile" component={ProfileBuilder} />
     </Switch>
   )
 }
